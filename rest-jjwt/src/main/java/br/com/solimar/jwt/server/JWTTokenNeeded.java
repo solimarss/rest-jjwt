6bde43cbb314bfa73bfa4bcab309fd2bcb93cbbd
Solimar Silva e Silva <solimar.silva@trt8.jus.br>
2017-11-30 15:02:54 -0300

    .

--- rest-jjwt/src/main/java/br/com/solimar/jwt/server/JWTTokenNeeded.java
+++ rest-jjwt/src/main/java/br/com/solimar/jwt/server/JWTTokenNeeded.java
@@ -9,4 +9,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface JWTTokenNeeded {
+	
+	//String role();
+	
 }
--- rest-jjwt/src/main/java/br/com/solimar/jwt/server/JWTTokenNeededFilter.java
+++ rest-jjwt/src/main/java/br/com/solimar/jwt/server/JWTTokenNeededFilter.java
@@ -14,28 +14,35 @@ import javax.ws.rs.ext.Provider;
 @JWTTokenNeeded
 @Priority(Priorities.AUTHENTICATION)
 public class JWTTokenNeededFilter implements ContainerRequestFilter {
- 
-    public void filter(ContainerRequestContext requestContext) throws IOException {
-    	
-    	System.out.println("Entrou no filter");
-    	
-    	// Get the HTTP Authorization header from the request
-        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
-        
- 
-        // Extract the token from the HTTP Authorization header
-       // String token = authorizationHeader.substring("Bearer".length()).trim();
-        String token = authorizationHeader.toString();
- 
-        try {
-             // Validate the token
-        	Jwt.validateJwt(token);
-            System.out.println("#### valid token : " + token);
- 
-        } catch (Exception e) {
-            //logger.severe("#### invalid token : " + token);
-            System.out.println("#### invalid token : " + token);
-            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
-        }
-    }
+
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+
+		System.out.println("Entrou no filter");
+
+		// Get the HTTP Authorization header from the request
+		String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+		if (authorizationHeader != null) {
+
+			// Extract the token from the HTTP Authorization header
+			// String token =
+			// authorizationHeader.substring("Bearer".length()).trim();
+			String token = authorizationHeader.toString();
+
+			try {
+				// Validate the token
+				Jwt.validateJwt(token);
+				System.out.println("#### valid token : " + token);
+
+			} catch (Exception e) {
+				// logger.severe("#### invalid token : " + token);
+				System.out.println("#### invalid token : " + token);
+				requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+			}
+
+		} else {
+			System.out.println("#### invalid token");
+			requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+		}
+	}
 }
